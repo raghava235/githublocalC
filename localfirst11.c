8d56c8ae4f4263f2bfcd6e6bbffe36c76fed58d9
Raghava235 <raghavendra.iv@gmail.com>
2024-04-21 18:20:43 +0530

    add local repo initial

---
+++ localfirst11.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() 
+{
+    printf("First C program: enjoy \n");
+    printf("working in Git");
+    return 0;
+}
---
+++ localsecond22.c
@@ -0,0 +1,6 @@
+# include <stdio.h>
+
+int main()
+{
+    printf("Hannsika is my daughter");
+}
